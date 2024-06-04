abab02a23bcf0c1f9d7e8be05b62c593e0e6e38c
CarolSimplicio <princarol2007@gmail.com>
2024-06-04 16:46:08 -0300

    link perfil add

--- frontend/src/Pages/Perfil.js
+++ frontend/src/Pages/Perfil.js
@@ -1,9 +1,7 @@
-//Lista de Usuarios
 import React from "react";
 import CadastroEndereco from "../Components/Endereco/CadastroEndereco";
 import TabelaEndereco from "../Components/Endereco/TabelaEndereco";
 import { Container, Row, Col } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table';
 
 const ListaUsuarios = () => {
     return (
@@ -17,27 +15,7 @@ const ListaUsuarios = () => {
                         <Col><h4>Olá, fulano...</h4></Col></Row>
                 </Container>
                 <Container>
-                    <Table striped bordered hover size="sm">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                            </tr>
-                        </tbody>
-                    </Table>
+                    <div className="local">Casa</div>
                 </Container>
             </div >
         </>
